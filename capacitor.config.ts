fb53241bec3fc35c1dbfab491341214a96c3ea1c
jonata-83 <116326562+jonata-83@users.noreply.github.com>
2026-06-21 20:43:47 -0300

    Add files via upload

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,10 @@ const config: CapacitorConfig = {
   appId: 'com.jonata.jlcaixa',
   appName: 'Caixa JL',
   webDir: 'www',
+  server: {
+    url: 'https://jonata-83.github.io/JL-caixa/',
+    cleartext: false
+  },
   android: {
     allowMixedContent: true
   }
